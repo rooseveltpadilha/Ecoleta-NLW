584e47faec9ee5332ce1062a8faba8fafa87594b
Roosevelt Padilha <roosevelttgp@gmail.com>
2020-06-07 13:09:50 -0300

    first commit

---
+++ project/server/src/server.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import cors from 'cors';
+import path from 'path';
+import routes from './routes';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+
+app.listen(process.env.SERVER_PORT, () => {
+  console.log(`[*] Server running on port: ${process.env.SERVER_PORT}`);
+});
